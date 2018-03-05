17190e87923a68119c9fcc6fcf303695b5cee8c1
Bet10 <f_154_i@hotmail.com>
2018-03-05 16:55:56 +0200

    sa
    as

--- IceCreamAutomation/Program.cs
+++ IceCreamAutomation/Program.cs
@@ -8,9 +8,7 @@ namespace IceCreamAutomation
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
